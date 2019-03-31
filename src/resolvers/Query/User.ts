bdcb4885b9537b844b5e893229de8d5f1a8a5161
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-03-30 21:08:35 -0400

    updated user queries, added doc comments

--- src/resolvers/Query/User.ts
+++ src/resolvers/Query/User.ts
@@ -1,16 +1,46 @@
 import { getUserId, Context } from "../../utils";
 
 export const User = {
-  usersHauls: ({ id }, args, ctx: Context) => {
-    return ctx.prisma.user({ id }).hauls();
-  },
-  me(parent, args, ctx: Context) {
+  /**
+   * Retrieves a user by their ID
+   * @param id - the ID of the user
+   */
+  user: async ({ id }, args, ctx: Context) => {
     const userId = getUserId(ctx);
 
     if (userId == null) {
       // TODO: do something after error is thrown
+      // TODO: Wrap this in method somewhere - too much duplication
     }
 
-    return ctx.prisma.user({ id: userId });
+    return await ctx.prisma.user({ id }).hauls();
+  },
+  /**
+   * Returns all users within the database
+   */
+  users: async (parent, args, ctx: Context) => {
+    const userId = getUserId(ctx);
+
+    if (userId == null) {
+      // TODO: do something after error is thrown
+      // TODO: Wrap this in method somewhere - too much duplication
+    }
+
+    return await ctx.prisma.users();
+  },
+
+  /**
+   * Get the users own data
+   * @param ctx -request context allows access to http headers
+   */
+  me(ctx: Context) {
+    const userId = getUserId(ctx);
+
+    if (userId == null) {
+      // TODO: do something after error is thrown
+      // TODO: Wrap this in method somewhere - too much duplication
+    }
+
+    return ctx.prisma.user({ id: userId }).hauls();
   }
 };
