63478506e5e7a8c1a10d64907aa566054b84ba3b
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-03-30 21:08:08 -0400

    updated haul queries, added doc comments

--- src/resolvers/Query/Haul.ts
+++ src/resolvers/Query/Haul.ts
@@ -1,65 +1,39 @@
 import { getUserId, Context } from "../../utils";
 
 export const HaulQueries = {
-  hauls(parent, args, ctx: Context) {
+  /**
+   * Returns an array of Haul object types
+   *
+   * @param ctx - request context allows access to http headers
+   */
+  hauls(ctx: Context) {
     const userId = getUserId(ctx);
 
     if (userId == null) {
       // TODO: do something after error is thrown
+      // TODO: Wrap this in method somewhere - too much duplication
     }
 
     return ctx.prisma.hauls;
   },
 
-  haulById(parent, { id }, ctx: Context) {
+  /**
+   * Returns a Haul object type by it's primary ID
+   *
+   * @param id - the ID of the Haul
+   * @param ctx - request context allows access to http headers
+   */
+  haul(parent, { id }, ctx: Context) {
     const userId = getUserId(ctx);
 
     if (userId == null) {
       // TODO: do something after error is thrown
+      // TODO: Wrap this in method somewhere - too much duplication
     }
     const where = {
       id: id
     };
 
     return ctx.prisma.haul(where);
-  },
-
-  items(parent, { id }, ctx: Context) {
-    const userId = getUserId(ctx);
-
-    if (userId == null) {
-      // TODO: do something after error is thrown
-    }
-    return ctx.prisma.items();
-  },
-
-  itemById(parent, { id }, ctx: Context) {
-    const userId = getUserId(ctx);
-
-    if (userId == null) {
-      // TODO: do something after error is thrown
-    }
-
-    return ctx.prisma.item({ id });
-  },
-
-  images(parent, { id }, ctx: Context) {
-    const userId = getUserId(ctx);
-
-    if (userId == null) {
-      // TODO: do something after error is thrown
-    }
-
-    return ctx.prisma.images();
-  },
-
-  image(parent, { id }, ctx: Context) {
-    const userId = getUserId(ctx);
-
-    if (userId == null) {
-      // TODO: do something after error is thrown
-    }
-
-    return ctx.prisma.image({ id });
   }
 };
